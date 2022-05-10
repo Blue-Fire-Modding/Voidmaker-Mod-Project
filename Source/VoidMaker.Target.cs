fabb0b97a41c57634dd3eb3ebd20841d945bbaf3
bananaturtlesandwich <afcarreyette@gmail.com>
2022-05-10 21:20:50 +0100

    OK it builds and everything now

---
+++ Source/VoidMaker.Target.cs
@@ -0,0 +1,11 @@
+﻿using UnrealBuildTool;
+
+public class VoidMakerTarget : TargetRules {
+	public VoidMakerTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"PROA34",
+		});
+	}
+}
